f7940d7a4dfe4a36cce20249c95abdb58bf0586a
Dominykas Plesevičius <dominykasplesevicius@gmail.com>
2026-03-08 18:51:57 +0200

    Add more decks

--- src/lib/constants/decks.ts
+++ src/lib/constants/decks.ts
@@ -1,8 +1,12 @@
 import type { Deck } from "$lib/types/cards";
+import { ARONSON_STACK } from "./aronson-stack";
 import { MNEMONICA } from "./mnemonica";
 import { REDFORD_STACK } from "./redford-stack";
+import { SI_STEBBINS } from "./si-stebbins";
 
 export const DECKS: Record<string, Deck> = {
   [MNEMONICA.id]: MNEMONICA,
   [REDFORD_STACK.id]: REDFORD_STACK,
+  [ARONSON_STACK.id]: ARONSON_STACK,
+  [SI_STEBBINS.id]: SI_STEBBINS,
 };
