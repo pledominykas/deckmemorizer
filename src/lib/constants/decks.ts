0b9432310ef2e83189b1defe4dc15daa3eb0bec4
Dominykas Plesevičius <dominykasplesevicius@gmail.com>
2026-03-08 19:38:46 +0200

    Add memorandum deck

--- src/lib/constants/decks.ts
+++ src/lib/constants/decks.ts
@@ -1,5 +1,6 @@
 import type { Deck } from "$lib/types/cards";
 import { ARONSON_STACK } from "./aronson-stack";
+import { MEMORANDUM } from "./memorandum";
 import { MNEMONICA } from "./mnemonica";
 import { REDFORD_STACK } from "./redford-stack";
 import { SI_STEBBINS } from "./si-stebbins";
@@ -9,4 +10,5 @@ export const DECKS: Record<string, Deck> = {
   [REDFORD_STACK.id]: REDFORD_STACK,
   [ARONSON_STACK.id]: ARONSON_STACK,
   [SI_STEBBINS.id]: SI_STEBBINS,
+  [MEMORANDUM.id]: MEMORANDUM,
 };
